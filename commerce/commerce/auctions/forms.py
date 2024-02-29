76607957f3a4bc66390fc5f5770516631811f034
NevenaAcevska <112829884+NevenaAcevska@users.noreply.github.com>
2024-02-29 13:19:06 +0100

    Delete commerce/commerce directory

--- commerce/commerce/auctions/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import Listing
-
-class CreateListingForm(forms.ModelForm):
-    class Meta:
-        model = Listing
-        fields = ['title', 'description', 'starting_bid', 'image_url', 'category']
